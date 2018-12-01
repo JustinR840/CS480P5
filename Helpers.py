54f99a39b90e999afafb934c48ab1187dc15e416
Elias James Mote <eliasjmote@gmail.com>
2018-11-30 19:11:32 -0800

    Hotfix: forgot to remove helper functions

--- Helpers.py
+++ Helpers.py
@@ -228,53 +228,6 @@ class Image:
 ############################ Multilayer perceptron helper functions ################################
 ####################################################################################################
 
-# Compute activation of hidden neurons
-def hidden_act(input_set, hidden_wgts, num_hidden_nodes, m):
-	hidden_acts = []
-	for i in range(num_hidden_nodes):
-		weighted_sum = np.dot(input_set[m], hidden_wgts[i])
-		hidden_acts.append(1 / (1 + math.exp(-1 * weighted_sum)))
-	return hidden_acts
-
-# Compute activation of output neurons
-def output_act(hidden_acts, output_wgts, num_hidden_nodes, num_output_nodes):
-	output_acts = []
-	for i in range(num_output_nodes):
-		weighted_sum = np.dot(hidden_acts, output_wgts[i])
-		output_acts.append(1 / (1 + math.exp(-1 * weighted_sum)))
-	return output_acts
-
-def compute_output_error(output_acts, num_output_nodes, target_wgts):
-	delta_o = []
-	for i in range(num_output_nodes):
-		delta_o.append((output_acts[i] - target_wgts[i]) * output_acts[i]
-			* ((1 - output_acts[i])))
-	return delta_o
-
-# Compute error at the hidden layer neurons
-def compute_hidden_error(hidden_acts, hidden_wgts, num_hidden_nodes, num_output_nodes, delta_o):
-	delta_h = []
-	for i in range(num_hidden_nodes):
-		for j in range(num_output_nodes):
-			weighted_sum = np.dot(hidden_wgts[i], delta_o[j])
-		delta_h.append(hidden_acts[i] * (1 - hidden_acts[i]) * weighted_sum)
-	return delta_h
-
-# Update output layer weights
-def update_output_wgts(hidden_acts, num_hidden_nodes, num_output_nodes, output_wgts, delta_o, eta):
-	new_output_wgts = deepcopy(output_wgts)
-	for i in range(num_output_nodes):
-		for j in range(num_hidden_nodes):
-			new_output_wgts[i][j] = (output_wgts[i][j] - eta * delta_o[i] * hidden_acts[j])
-	return new_output_wgts
-
-def update_hidden_wgts(input_set, num_hidden_nodes, hidden_wgts, delta_h, eta, m):
-	new_hidden_wgts = deepcopy(hidden_wgts)
-	for i in range(num_hidden_nodes):
-		for j in range(len(input_set[0])):
-			hidden_wgts[i][j] = (hidden_wgts[i][j] - eta * delta_h[i][j] * input_set[m][j])
-	return new_hidden_wgts
-
 ####################################################################################################
 ############################ Multilayer perceptron helper functions ################################
 ####################################################################################################
